7c055f4febf45481b1492f4efef47b237e1faba1
Prescott Breeden <prescott.breeden@gmail.com>
2018-05-07 11:58:24 -0700

    getting closer...

--- server.js
+++ server.js
@@ -21,65 +21,93 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+
 app.use(favicon(path.join(__dirname, './public', 'images', 'user-favicon.ico')));
 app.use(express.static(path.join(__dirname, './public')));
 app.use(express.static(path.join(__dirname, './bower_components')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-mongoose.connect('mongodb://localhost:27017/login_reg');
+
+mongoose.connect('mongod://localhost:27017/login_reg')
+    .then(() => {
+        console.log("connected to db");
+    })
+    .catch(() => {
+        console.log("unable to connect to database");
+    })
 
 // user schema
 const UserSchema = new mongoose.Schema({
-    first_name: {type: String, required: true},
-    last_name: {type: String, required: true},
-    email: {type: String, required: true, lowercase: true, unique: true},
+    first_name: {type: String, required: [true, 'First name cannot be blank']},
+    last_name: {type: String, required: [true, 'Last name cannot be blank']},
+    email: {type: String, required: [true, 'Email cannot be blank'], lowercase: true, unique: true},
     password: {type: String, required: true}
 }, {timestamps: true})
 
 mongoose.model('User', UserSchema);
 const User = mongoose.model('User');
 
-app.get('/', function(req, res, next){
+app.get('/', (req, res, next) => {
     res.render('index');
 })
 
 
-app.post('/register', function(req, res, next) {
+app.post('/register', (req, res, next) => {
     // new user registration
     var password = req.body.password;
     var cpassword = req.body.cpassword;
     if(password != cpassword)
     {
         console.log('passwords do not match')
-        res.redirect('/failed');
+        res.render('failed', {errors: "passwords do not match"});
     }
     else
     {
+        bcrypt.hash(password, 10)
+            .then((hash) => {    
+                password = hash;
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+
         var user = new User({
             first_name: req.body.first_name,
             last_name: req.body.last_name,
             email: req.body.email,
-            password: hash
+            password: password
         })
 
-        user.save(function(err) {
-            if(err) {
-                res.redirect('/failed', {errors: err});
-                console.log(err)
-            } else {
+        user.save()
+            .then((user) => {
                 console.log('successfully added a user!')
-                res.redirect('/success');
-            }
-        })
-
-        bcrypt.hash(password, 10, function(err, hash) {    
-   
-        })
+                console.log(user);
+                res.render('success', {user: user});
+            })
+            .catch((err) => {
+                res.render('failed', {errors: err});
+                console.log(err);
+            })
     }
 })
+
 // user login
-app.post('/login', function(req, res, next) {
-    res.redirect('/');
+app.post('/login', (req, res, next) => {
+    User.findOne({email: req.body.email})
+        .then((user) => {
+            if(user.password !== req.body.password)
+            {
+                console.log("password doesn't match stored password")
+            }
+            else
+            {
+                console.log(`${user.name} has logged in to server`);
+                res.render('success', {user: user});
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 })
 
 // user in session redirect to success
